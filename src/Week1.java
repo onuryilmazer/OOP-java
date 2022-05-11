7ed487ae9750a28fb45d9b6796b1013ada0c9a8a
Onur Yılmazer <onur.yilmazer1@gmail.com>
2022-05-11 23:22:11 +0200

    added task 2

--- src/Week1.java
+++ src/Week1.java
@@ -1,14 +1,51 @@
+/*
+Aufgabe 1:
+a) [1, 2, 17, 4, -1, -2, -3, -4]
+b)
+	Teil A:
+        Fehler in Zeile: #9, auf a[a.length] kann man nicht zugreifen, "out of bounds".
+        Richtig: for (int i=0;i<a.length;i++)
+	Teil B:
+        Fehler in Zeile:---
+        Richtig:---
+	Teil C:
+        Fehler in Zeile: #23 Iterator "i" beginnt bei 1, m[0] würde so übersprungen werden und außerdem würde man am Ende außerhalb des Felds treten.
+        Richtig: m[i-1]=val / 3;
+	Teil D:
+        Fehler in Zeile: #27. Wenn alle Werte negativ sind ist die Ausgabe falsch.
+        Richtig: int max = Integer.MIN_VALUE;
+	Teil E:
+        Fehler in Zeile: #36 Erstes von Null "verschiedenes" Element ist gesucht.
+        Richtig: (i<a.length && a[i]==0) i++;
+
+c) private static void splitAccordingToIndex(int[] numbers) ----- unten.
+d) private static void splitAccordingToEvenness(int[] numbers) ----- unten.
+
+*/
+
+
 public class Week1 {
     public static void main(String[] args) {
-        int[] numbers = {0,1,2,3,4,5,6,7,8,9};
+
+        //Aufgabe 1c
+        int[] numbers = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
         System.out.println("Odd and even indexed elements split into two arrays: ");
         splitAccordingToIndex(numbers);
 
         numbers = new int[]{5, 3, 1, 4, 6, 2, 8};
 
-        System.out.println("Odd and even elements split into two arrays: ");
+        //Aufgabe 1d
+        System.out.println("Odd and even elements of the array split into two arrays: ");
         splitAccordingToEvenness(numbers);
+
+
+        //Aufgabe 2
+        int i = 0;
+        java.util.Scanner s = new java.util.Scanner((System.in));
+        System.out.print("Which Fibonacci number do you want?: ");
+        i = s.nextInt();
+        System.out.println(i + ". Fibonacci number: " + fibonacci(i));
     }
 
     private static void splitAccordingToIndex(int[] numbers) {
@@ -69,5 +106,27 @@ public class Week1 {
             System.out.print(number + " ");
         }
         System.out.println("");
+    }
+
+    private static int fibonacci(int n) {
+        if (n<0) {
+            System.exit(0);
+            return -1;
+        }
+        else if (n < 2) {
+            return n;
+        }
+
+        int num1 = 0;
+        int num2 = 1;
+        int num3 = 1;
+
+        for (int i=2;i<=n;i++) {
+            num3 = num2 + num1;
+            num1 = num2;
+            num2 = num3;
+        }
+
+        return num3;
     }
 }
