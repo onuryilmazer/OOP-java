439d2673d1f7dc221dd0378959a148aaf310fefb
Onur Yılmazer <onur.yilmazer1@gmail.com>
2022-05-12 23:56:18 +0200

    added unit tests for the value-returning functions

---
+++ tests/Week1Tests.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+
+public class Week1Tests {
+    @Test
+    @DisplayName("Function should output correct values: ")
+    public void testFibonacci() {
+        assertEquals(0, Week1.fibonacci(0));
+        assertEquals(1, Week1.fibonacci(1));
+        assertEquals(1, Week1.fibonacci(2));
+        assertEquals(2, Week1.fibonacci(3));
+
+        assertEquals(8, Week1.fibonacci(6));
+    }
+
+    @Test
+    @DisplayName("Function should output correct values: ")
+    public void testProst() {
+        assertEquals(0, Week1.prost(0));
+        assertEquals(0, Week1.prost(1));
+        assertEquals(1, Week1.prost(2));
+        assertEquals(3, Week1.prost(3));
+        assertEquals(6, Week1.prost(4));
+        assertEquals(10, Week1.prost(5));
+    }
+}
