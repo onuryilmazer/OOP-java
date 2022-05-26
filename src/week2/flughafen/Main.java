ad1299f8117a21e7ca66342d82b9183247089ff0
Onur Yılmazer <onur.yilmazer1@gmail.com>
2022-05-26 21:11:29 +0200

    -

--- src/week2/flughafen/Main.java
+++ src/week2/flughafen/Main.java
@@ -2,10 +2,10 @@ package week2.flughafen;
 
 public class Main {
     public static void main(String args[]) {
-        Airport airport1 = new Airport(10);
+        Airport airport1 = new Airport(3);
 
         airport1.addNewFlight(new Flight(1, "Sylt", "B1", "15:00", false));
-        airport1.addNewFlight(new Flight(1, "Sylt", "B1", "15:00", false));
+        airport1.addNewFlight(new Flight(1, "Sylt", "B1", "15:00", false));  //adding twice.
 
         airport1.addNewFlight(new Flight(2, "Amsterdam", "B2", "15:00", true));
         airport1.addNewFlight(new Flight(3, "Paris", "B3", "15:00", true));
@@ -19,7 +19,7 @@ public class Main {
         System.out.println("-------------------------");
 
         airport1.removeFlight(3);
-        airport1.removeFlight(3);
+        airport1.removeFlight(3); //removing twice.
         airport1.listDeparturesOnScreen();
         airport1.listArrivalsOnScreen();
     }
